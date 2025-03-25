1a0866f9645badd432f0abe991f936ac8daa3a99
Swallowedin <charles.thomasse@gmail.com>
2025-03-25 09:54:31 +0100

    Create __init__.py

---
+++ ui/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module de gestion de l'interface utilisateur de l'application.
+"""
+from ui.sidebar import render_sidebar
+from ui.tabs import render_input_tabs
+from ui.results import display_results
