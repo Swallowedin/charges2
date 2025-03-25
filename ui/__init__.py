71830dea4dc1c5715c0edffd303e2d7d5f0629e0
Swallowedin <charles.thomasse@gmail.com>
2025-03-25 10:16:21 +0100

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,6 +1,4 @@
 """
 Module de gestion de l'interface utilisateur de l'application.
 """
-from ui.sidebar import render_sidebar
-from ui.tabs import render_input_tabs
-from ui.results import display_results
+
